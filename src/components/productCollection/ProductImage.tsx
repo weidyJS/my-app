0282049f297dc62ee88e3d0823bae87b35689135
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-10 14:23:50 +0800

    feat: Link 与动态导航

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, Typography } from "antd";
-import { withRouter, RouteComponentProps } from "react-router-dom";
+import { withRouter, RouteComponentProps, Link } from "react-router-dom";
 
 interface PropsType extends RouteComponentProps {
   id: string | number;
@@ -21,7 +21,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
   match
 }) => {
   return (
-    <div onClick={() => history.push(`detail/${id}`) }>
+    <Link to={`detail/${id}`}>
       {size === "large" ? (
         <Image src={imageSrc} height={285} width={490} />
       ) : (
@@ -33,7 +33,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
           ¥ {price} 起
         </Typography.Text>
       </div>
-    </div>
+    </Link>
   );
 };
 
