0e096326a9ed5ef45bcb217ae7d6fa37bb0d5595
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-10 21:05:20 +0800

    feat:【路由架构】基础路由及页面导航

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages';
+import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages';
 
 function App() {
   return (
@@ -9,6 +9,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/detail/:touristRouteId" element={<DetailPage />} />
+          <Route path="*" element={<h1>404 not found 页面去火星了</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+type MatchParams = {
+	touristRouteId : string,
+	other : string
+}
 
-interface MatchParams {
+interface MatchParams2 {
   touristRouteId: string,
   other : string
 }
 
 export const DetailPage: React.FC = () => {
-  var params = useParams<keyof MatchParams>();
+  // var params = useParams<"touristRouteId">();
+  // var params = useParams<MatchParams>();
+  var params = useParams<keyof MatchParams2>();
   return <h1>旅游路线详情页, 路线id: {params.touristRouteId} {params.other}</h1>;
 };
