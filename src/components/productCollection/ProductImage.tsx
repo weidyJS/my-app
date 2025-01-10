fe232679dd7108909b57886205f0c9faaac298b2
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-10 14:12:32 +0800

    feat: 【路由搭建】withRouter与useRouter

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image, Typography } from "antd";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
-interface PropsType {
+interface PropsType extends RouteComponentProps {
   id: string | number;
   size: "large" | "small";
   imageSrc: string;
@@ -9,22 +10,31 @@ interface PropsType {
   title: string;
 }
 
-export const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, title}) => {
+const ProductImageComponent: React.FC<PropsType> = ({
+  id,
+  size,
+  imageSrc,
+  price,
+  title,
+  history,
+  location,
+  match
+}) => {
   return (
-    <>
+    <div onClick={() => history.push(`detail/${id}`) }>
       {size === "large" ? (
         <Image src={imageSrc} height={285} width={490} />
       ) : (
         <Image src={imageSrc} height={120} width={240} />
       )}
       <div>
-        <Typography.Text type="secondary">
-          {title.slice(0, 25)}
-        </Typography.Text>
+        <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
         <Typography.Text type="danger" strong>
           ¥ {price} 起
         </Typography.Text>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export const ProductImage = withRouter(ProductImageComponent);
