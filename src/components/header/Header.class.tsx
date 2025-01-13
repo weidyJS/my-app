03ab36cada5f45eab163cfcde624d37aac663fcc
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-14 00:19:49 +0800

    feat: 【i18n】完成网站语言切换

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -7,10 +7,11 @@ import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { withRouter, RouteComponentProps } from "../../helpers/withRouter";
 import store from "../../redux/store";
 import { LanguageState } from "../../redux/languageReducer";
+import { withTranslation, WithTranslation } from "react-i18next";
 
 interface State extends LanguageState {}
 
-class HeaderComponent extends React.Component<RouteComponentProps, State> {
+class HeaderComponent extends React.Component<RouteComponentProps & WithTranslation, State> {
   constructor(props) {
     super(props);
     const storeState = store.getState();
@@ -47,12 +48,12 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
   };
 
   render(): React.ReactNode {
-    const { navigate } = this.props;
+    const { navigate, t } = this.props;
     return (
       <div className={styles["app-header"]}>
         <div className={styles["top-header"]}>
           <div className={styles.inner}>
-            <Typography.Text>让旅游更幸福</Typography.Text>
+            <Typography.Text>{t("header.slogan")}</Typography.Text>
             <Dropdown.Button
               style={{ marginLeft: 15, display: 'inline-flex', width: 'initial' }}
               overlay={
@@ -62,7 +63,7 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
                     ...this.state.languageList.map((l) => {
                       return { key: l.code, label: l.name };
                     }),
-                    { key: "new", label: "添加新语言" },
+                    { key: "new", label: t("header.add_new_language") },
                   ]}
                 />
               }
@@ -71,8 +72,8 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
               {this.state.language === "zh" ? "中文" : "English"}
             </Dropdown.Button>
             <Button.Group className={styles["button-group"]}>
-              <Button onClick={() => navigate("/register")}>注册</Button>
-              <Button onClick={() => navigate("/signin")}>登陆</Button>
+              <Button onClick={() => navigate("/register")}>{t("header.register")}</Button>
+              <Button onClick={() => navigate("/signin")}>{t("header.signin")}</Button>
             </Button.Group>
           </div>
         </div>
@@ -80,7 +81,7 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
           <span onClick={() => navigate("/")}>
             <img src={logo} alt="logo" className={styles["App-logo"]} />
             <Typography.Title level={3} className={styles.title}>
-              React旅游网
+            {t("header.title")}
             </Typography.Title>
           </span>
           <Input.Search
@@ -92,22 +93,22 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
           mode={"horizontal"}
           className={styles["main-menu"]}
           items={[
-            { key: "1", label: "旅游首页" },
-            { key: "2", label: "周末游" },
-            { key: "3", label: "跟团游" },
-            { key: "4", label: "自由行" },
-            { key: "5", label: "私家团" },
-            { key: "6", label: "邮轮" },
-            { key: "7", label: "酒店+景点" },
-            { key: "8", label: "当地玩乐" },
-            { key: "9", label: "主题游" },
-            { key: "10", label: "定制游" },
-            { key: "11", label: "游学" },
-            { key: "12", label: "签证" },
-            { key: "13", label: "企业游" },
-            { key: "14", label: "高端游" },
-            { key: "15", label: "爱玩户外" },
-            { key: "16", label: "保险" },
+            { key: "1", label: t("header.home_page") },
+            { key: "2", label: t("header.weekend") },
+            { key: "3", label: t("header.group") },
+            { key: "4", label: t("header.backpack") },
+            { key: "5", label: t("header.private") },
+            { key: "6", label: t("header.cruise") },
+            { key: "7", label: t("header.hotel") },
+            { key: "8", label: t("header.local") },
+            { key: "9", label: t("header.theme") },
+            { key: "10", label: t("header.custom") },
+            { key: "11", label: t("header.study") },
+            { key: "12", label: t("header.visa") },
+            { key: "13", label: t("header.enterprise") },
+            { key: "14", label: t("header.high_end") },
+            { key: "15", label: t("header.outdoor") },
+            { key: "16", label: t("header.insurance") },
           ]}
         />
       </div>
@@ -115,4 +116,4 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
   }
 }
 
-export const Header = withRouter(HeaderComponent);
+export const Header = withTranslation()(withRouter(HeaderComponent));
--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,3 +1,5 @@
+import i18n from "i18next";
+
 export interface LanguageState {
   language: "en" | "zh";
   languageList: { name: string; code: string }[];
@@ -14,6 +16,7 @@ const defaultState: LanguageState = {
 const languageReducer = (state = defaultState, action) => {
   switch (action.type) {
     case "change_language":
+      i18n.changeLanguage(action.payload); // 这样处理是不标准的，有副作用
       return { ...state, language: action.payload };
     case "add_language":
       return {
