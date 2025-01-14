8590c5b655de9fd4b119d82a07e32f283b22a382
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-14 12:47:23 +0800

    feat: 【redux重构】action 的拆分与统一

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -6,8 +6,12 @@ import { GlobalOutlined } from "@ant-design/icons";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { withRouter, RouteComponentProps } from "../../helpers/withRouter";
 import store from "../../redux/store";
-import { LanguageState } from "../../redux/languageReducer";
+import { LanguageState } from "../../redux/language/languageReducer";
 import { withTranslation, WithTranslation } from "react-i18next";
+import {
+  addLanguageActionCreator,
+  changeLanguageActionCreator,
+} from "../../redux/language/languageActions";
 
 interface State extends LanguageState {}
 
@@ -33,16 +37,10 @@ class HeaderComponent extends React.Component<RouteComponentProps & WithTranslat
   menuClickHandler = (e) => {
     console.log(e);
     if (e.key === "new") {
-      const action = {
-        type: "add_language",
-        payload: { code: "new_lang", name: "新语言" },
-      };
+      const action = addLanguageActionCreator("新语言", "new_lang")
       store.dispatch(action);
     } else {
-      const action = {
-        type: "change_language",
-        payload: e.key,
-      };
+      const action = changeLanguageActionCreator(e.key)
       store.dispatch(action);
     }
   };
