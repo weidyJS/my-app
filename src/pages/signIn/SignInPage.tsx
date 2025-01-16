69a4062d5f0d9708115975e0e6cb3ba74eebab1b
shengwei_jin <shengwei_jin@idgcapital.com>
2025-01-16 22:15:46 +0800

    feat: 【布局重构】页面布局

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { UserLayout } from "../../layouts/userLayout";
 
 export const SignInPage: React.FC = (props) => {
-  console.log(props)
-  return <h1>登录页面</h1>;
-}
+  return (
+    <UserLayout>
+      <h1>登录页面</h1>
+    </UserLayout>
+  );
+};
